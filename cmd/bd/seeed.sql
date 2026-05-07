7b462fec9f4b3cad463baa86a01944789bb31093
Tomas Alejandro Angarita Candia <tangaritac@unal.edu.co>
2026-05-06 19:02:18 -0500

    Add files via upload

---
+++ cmd/bd/seeed.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO role (id, name, description) VALUES 
+(1, 'Administrador' ,' Administrador General'),
+(2, 'Bibliotecario', 'Gestor de préstamos y devoluciones, actualización de ejemplares y gestión operativa de penalizaciones.'),
+(3, 'Estudiante', 'Usuario final. Consulta de catálogo, historial personal, reserva de puestos de estudio y préstamos.');
