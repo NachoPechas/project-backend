e19eab170309361c420cfa1de70feadd7d16d744
Juan Cristiano <juanestebancristianorinncon@gmail.com>
2026-07-05 16:10:49 -0500

    Cambios pero conservando user de antes

--- src/models/user.js
+++ src/models/user.js
@@ -4,51 +4,50 @@ const sequelize = require('../config/database');
 class Usuario extends Model {}
 
 Usuario.init(
-{
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  nombre: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-      len: [2, 100],
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [2, 100],
+      }
+    },
+    email: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      }
+    },
+    activo: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+
+    rol: {
+      type: DataTypes.STRING(30),
+      defaultValue: 'estudiante',
+      allowNull: false
+    },
+    proveedorAuth: {
+      type: DataTypes.STRING(20),
+      defaultValue: 'local',
+      allowNull: false
     }
   },
-  email: {
-    type: DataTypes.STRING(150),
-    allowNull: false,
-    unique: true, 
-    validate: {
-      isEmail: true, 
-    }
-  },
-  activo: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true,
-  },
-  // ── CAMBIO 1: Rol para la Biblioteca ──
-  rol: {
-    type: DataTypes.STRING(30),
-    defaultValue: 'estudiante', // Por defecto todos entran como estudiantes
-    allowNull: false
-  },
-  // ── CAMBIO 2: Para saber si se registró por Google/UNAL o manual ──
-  proveedorAuth: {
-    type: DataTypes.STRING(20),
-    defaultValue: 'local', // 'local' para los rama main, 'google' mi rama
-    allowNull: false
+  {
+    sequelize,
+    modelName: 'Usuario',
+    tableName: 'usuarios',
+    timestamps: true,
   }
-},
-{
-  sequelize, 
-  modelName: 'Usuario',
-  tableName: 'usuarios', 
-  timestamps: true, 
-}
 );
 
 module.exports = Usuario;
