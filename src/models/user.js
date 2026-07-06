459a905e3991bf7678c3cf93b8e93a601cb209a8
Juan Cristiano <juanestebancristianorinncon@gmail.com>
2026-07-06 14:41:44 -0500

    commit antes de merge con main

--- src/models/user.js
+++ src/models/user.js
@@ -43,7 +43,9 @@ User.init(
     sequelize,
     modelName: 'User',
     tableName: 'user',
-    timestamps: false,
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
   }
 );
 
